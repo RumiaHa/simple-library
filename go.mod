d605dc3cdc4ce252c39be60812ed4fc027329d41
Sofia-rgbi <tedeeva.s06@icloud.com>
2025-10-13 14:07:42 +0300

    feat: add initial project structure and data models

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RumiaHa/simple-library.git
+
+go 1.25.1
---
+++ models.go
@@ -0,0 +1,13 @@
+package main
+
+type Boook struct{
+	ID int
+	Title string
+	Author string
+	IsIssued bool
+}
+
+type Reader struct{
+	ID int
+	Name string
+}
