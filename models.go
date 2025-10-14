9819d74690a71fc2172f73910af35af42fcdbf80
RumiaHa <rumiahaitani@gmail.com>
2025-10-14 13:07:15 +0300

    1.Add ReturnBook and IssueBook in models.go

--- models.go
+++ models.go
@@ -2,19 +2,23 @@ package main
 
 import "fmt"
 
-type Boook struct {
-	ID       int
-	Title    string
-	Author   string
-	IsIssued bool
-}
-
 type Reader struct {
 	ID        int
 	FirstName string
 	LastName  string
 	IsActive  bool
 }
+
+
+type Book struct {
+	ID       int
+	Title    string
+	Author   string
+	Year int
+	IsIssued bool
+
+}
+
 
 //Выводит в консоль информацию о читателе
 func (r Reader) DisplayReader() {
@@ -35,4 +39,32 @@ func (r Reader) String() string{
 		status = "Не активен"
 	}
 	return fmt.Sprintf("Пользователь %s %s, ID: %d, пользователь:%",r.FirstName,r.LastName,r.ID,status)
+}
+
+func (b Book) String() string {
+	return fmt.Sprintf(`"%s (%s, %d)"`, b.Title, b.Author, b.Year)
+}
+
+func (b *Book) IssueBook() (r Reader) {
+	if b.IsIssued {
+		fmt.Printf("Книга %s уже кому-то выдана", b.Title)
+		return
+	}   
+	  if !r.IsActive {
+		fmt.Printf("Читатель %s %s не активен и не      может получить книгу.", r.FirstName, r.LastName)
+		return
+	}
+
+	b.IsIssued = true
+	fmt.Printf("Книга %s была выдана\n", b.Title)
+	return
+}
+
+func (b *Book) ReturnBook() {
+	if !b.IsIssued {
+		fmt.Printf("Книга %s уже в библиотеке", b.Title)
+		return
+	}
+	b.IsIssued = false
+	fmt.Printf("Книга %s возвращена в библиотеку\n", b.Title)
 }
