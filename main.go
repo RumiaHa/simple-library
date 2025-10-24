bf8fc33450ac4dd35a767f0323a7088cce7b13de
RumiaHa <rumiahaitani@gmail.com>
2025-10-24 12:07:54 +0300

    Redacted models.go, main.go

--- main.go
+++ main.go
@@ -5,84 +5,125 @@ import "fmt"
 func main() {
     fmt.Println("Запуск системы управления библиотекой...")
 
-    // 1. Создаем экземпляр библиотеки
-    myLibrary := &Library{} // Пустая библиотека готова к работе
+    // Создаем экземпляр библиотеки
+    myLibrary := &Library{}
 
     fmt.Println("\n--- Наполняем библиотеку ---")
 
-    // 2. Добавляем читателей
+    // Добавляем читателей
     myLibrary.AddReader("Агунда", "Кокойты")
     myLibrary.AddReader("Сергей", "Меняйло")
 
-    // 3. Добавляем книги
+    // Добавляем книги
     myLibrary.AddBook("1984", "Джордж Оруэлл", 1949)
+    myLibrary.AddBook("Мастер и Маргарита", "Михаил Булгаков", 1967)
 
-	    myLibrary.AddBook("Мастер и Маргарита", "Михаил Булгаков", 1967)
-
-
-
-
-		
     fmt.Println("\n--- Библиотека готова к работе ---")
-    fmt.Println("\nКоличество читателей:", len(myLibrary.Readers))
-    fmt.Println("\nКоличество книг:", len(myLibrary.Books))
+    fmt.Println("Количество читателей:", len(myLibrary.Readers))
+    fmt.Println("Количество книг:", len(myLibrary.Books))
 
+    fmt.Println("\n--- Список всех книг в библиотеке ---")
+    myLibrary.ListAllBooks()
 
-	fmt.Println("\n Список всех книг в библиотеке:")
-	myLibrary.ListAllBooks()
+    // Сценарий 1: Успешная выдача книги
+    fmt.Println("\n--- Сценарий 1: Успешная выдача книги ---")
+    err := myLibrary.IssueBookToReader(1, 1)
+    if err != nil {
+        fmt.Println("Ошибка выдачи книги:", err)
+    } else {
+        fmt.Println("Книга успешно выдана читателю")
+        
+        // Проверяем статус книги после выдачи
+        book, _ := myLibrary.FindBookByID(1)
+        if book != nil {
+            fmt.Println("Статус книги после выдачи:", book)
+        }
+    }
 
+    // Сценарий 2: Попытка выдать уже выданную книгу
+    fmt.Println("\n--- Сценарий 2: Попытка выдать уже выданную книгу ---")
+    err = myLibrary.IssueBookToReader(1, 2)
+    if err != nil {
+        fmt.Println("Ошибка выдачи книги:", err)
+    } else {
+        fmt.Println("Книга успешно выдана читателю")
+    }
 
+    // Сценарий 3: Попытка выдать книгу несуществующему читателю
+    fmt.Println("\n--- Сценарий 3: Попытка выдать книгу несуществующему читателю ---")
+    err = myLibrary.IssueBookToReader(1, 99)
+    if err != nil {
+        fmt.Println("Ошибка выдачи книги:", err)
+    } else {
+        fmt.Println("Книга успешно выдана читателю")
+    }
 
-fmt.Println("---Тестируем выдачу книг---")
-//Выдаем книгу 1 читателю 1
-err := myLibrary.IssueBookToReader(1, 1)
-if err != nil {
-	fmt.Println("Ошибка выдачи", err)
-}
+    // Сценарий 4: Попытка выдать несуществующую книгу
+    fmt.Println("\n--- Сценарий 4: Попытка выдать несуществующую книгу ---")
+    err = myLibrary.IssueBookToReader(99, 1)
+    if err != nil {
+        fmt.Println("Ошибка выдачи книги:", err)
+    } else {
+        fmt.Println("Книга успешно выдана читателю")
+    }
 
-//Проверить статус книги после выдачи
-book, _ := myLibrary.FindBookByID(1)
-if book != nil {
-	fmt.Println("Статус книги после выдачи:", book)
-}
+    // Сценарий 5: Успешный возврат книги
+    fmt.Println("\n--- Сценарий 5: Успешный возврат книги ---")
+    err = myLibrary.ReturnBook(1)
+    if err != nil {
+        fmt.Println("Ошибка возврата книги:", err)
+    } else {
+        fmt.Println("Книга успешно возвращена в библиотеку")
+        
+        // Проверяем статус книги после возврата
+        book, _ := myLibrary.FindBookByID(1)
+        if book != nil {
+            fmt.Println("Статус книги после возврата:", book)
+        }
+    }
 
-//Попытка выдать несуществующую книгу
-err = myLibrary.IssueBookToReader(99, 1)
-if err != nil {
-	fmt.Println("Ожидаемая ошибка:", err)
-}
+    // Сценарий 6: Попытка вернуть книгу, которая уже в библиотеке
+    fmt.Println("\n--- Сценарий 6: Попытка вернуть книгу, которая уже в библиотеке ---")
+    err = myLibrary.ReturnBook(1)
+    if err != nil {
+        fmt.Println("Ошибка возврата книги:", err)
+    } else {
+        fmt.Println("Книга успешно возвращена в библиотеку")
+    }
 
-err1 := myLibrary.ReturnBook(1)
-if  err1 != nil {
-    fmt.Println("Ошибка при выдаче книги: ", err)
-} else {
-    fmt.Println("Книга возвращена успешно")
-}
-err2 := myLibrary.ReturnBook(1)
-if  err2 != nil {
-    fmt.Println("Ошибка при выдаче книги: ", err)
-} else {
-    fmt.Println("Книга возвращена успешно")
-}
+    // Сценарий 7: Попытка вернуть несуществующую книгу
+    fmt.Println("\n--- Сценарий 7: Попытка вернуть несуществующую книгу ---")
+    err = myLibrary.ReturnBook(99)
+    if err != nil {
+        fmt.Println("Ошибка возврата книги:", err)
+    } else {
+        fmt.Println("Книга успешно возвращена в библиотеку")
+    }
 
+    // Тестирование функции GetPortFromConfig
+    fmt.Println("\n--- Тестирование функции GetPortFromConfig ---")
+    
+    // Тест 1: Конфиг без порта
+    config1 := map[string]string{
+        "NOPORT": "2132",
+    }
+    port, err := GetPortFromConfig(config1)
+    if err != nil {
+        fmt.Println("Ошибка чтения порта:", err)
+    } else {
+        fmt.Println("Порт номер:", port)
+    }
 
-//config := map[string]string {
-//    "PORT" : "2123",
-//}
+    // Тест 2: Конфиг с портом
+    config2 := map[string]string{
+        "PORT": "8080",
+    }
+    port, err = GetPortFromConfig(config2)
+    if err != nil {
+        fmt.Println("Ошибка чтения порта:", err)
+    } else {
+        fmt.Println("Порт номер:", port)
+    }
 
-
-config1 := map[string]string {
-    "NOPORT" : "2132",
-}
-
-port, err := GetPortFromConfig(config1)
-if err != nil {
-    fmt.Println("Произошла ошибка чтения порта:", err)
-}else {
-    fmt.Println("Порт номер:", port)
-}
-
-
-
-
+    fmt.Println("\n--- Работа системы управления библиотекой завершена ---")
 }
--- models.go
+++ models.go
@@ -9,135 +9,126 @@ type Reader struct {
 	IsActive  bool
 }
 
-
 type Book struct {
 	ID       int
 	Title    string
 	Author   string
-	Year int
+	Year     int
 	IsIssued bool
 	ReaderId *int
 }
 
-
-type Library struct {	
-	Books []*Book
-	Readers []*Reader
-
-	lastBookID int
+type Library struct {
+	Books        []*Book
+	Readers      []*Reader
+	lastBookID   int
 	LastReaderID int
 }
 
-func (lib *Library) AddReader(firstName,lastName string) *Reader{
+func (lib *Library) AddReader(firstName, lastName string) *Reader {
 	lib.LastReaderID++
 
 	newReader := &Reader{
-		ID: lib.LastReaderID,
+		ID:        lib.LastReaderID,
 		FirstName: firstName,
-		LastName: lastName,
-		IsActive: true,
+		LastName:  lastName,
+		IsActive:  true,
 	}
 
-
-	lib.Readers = append(lib.Readers,newReader)
-	fmt.Printf("Зарегистрирован новый читатель: %s\n", newReader)
+	lib.Readers = append(lib.Readers, newReader)
 	return newReader
 }
 
-func (lib *Library) AddBook(title, author string, year int) *Book{
+func (lib *Library) AddBook(title, author string, year int) (*Book, error) {
+
+	for _, b :=  range lib.Books{
+		if b.Title == title && b.Author == author {
+			return nil, fmt.Errorf("книга '%s' автора '%s' уже существует", title, author)
+		}
+	}
+
+
 	lib.lastBookID++
 
 	newBook := &Book{
-		ID: lib.lastBookID,
-		Title: title,
-		Author: author,
-		Year: year,
+		ID:       lib.lastBookID,
+		Title:    title,
+		Author:   author,
+		Year:     year,
 		IsIssued: false,
 	}
 
+
+
 	lib.Books = append(lib.Books, newBook)
-	fmt.Printf("Добавлена новая книга: %s\n", newBook)
-	return newBook
+	return newBook, nil
 }
-
-
 
 func (lib *Library) FindBookByID(id int) (*Book, error) {
-    for _, book := range lib.Books {
-        if book.ID == id {
-            return book, nil
-        }
-    }
-    return nil, fmt.Errorf("книга с ID %d не найдена", id)
+	for _, book := range lib.Books {
+		if book.ID == id {
+			return book, nil
+		}
+	}
+	return nil, fmt.Errorf("книга с ID %d не найдена", id)
 }
 
-
-
 func (lib *Library) FindReaderByID(id int) (*Reader, error) {
-    for _, reader := range lib.Readers {
-        if reader.ID == id {
-            return reader, nil
-        }
-    }
-    return nil, fmt.Errorf("читатель с ID %d не найден", id)
+	for _, reader := range lib.Readers {
+		if reader.ID == id {
+			return reader, nil
+		}
+	}
+	return nil, fmt.Errorf("читатель с ID %d не найден", id)
 }
 
 func (lib *Library) IssueBookToReader(bookID int, readerID int) error {
-    book, err := lib.FindBookByID(bookID)
-    if err != nil {
-        return err
-    }
+	book, err := lib.FindBookByID(bookID)
+	if err != nil {
+		return err
+	}
 
-    reader, err := lib.FindReaderByID(readerID)
-    if err != nil {
-        return err
-    }
+	reader, err := lib.FindReaderByID(readerID)
+	if err != nil {
+		return err
+	}
 
-    if book.IsIssued {
-        return fmt.Errorf("книга %s уже выдана", book.Title)
-    }
+	if book.IsIssued {
+		return fmt.Errorf("книга %s уже выдана", book.Title)
+	}
 
-    if !reader.IsActive {
-        return fmt.Errorf("читатель %s %s не активен", reader.FirstName, reader.LastName)
-    }
+	if !reader.IsActive {
+		return fmt.Errorf("читатель %s %s не активен", reader.FirstName, reader.LastName)
+	}
 
-    book.IsIssued = true
-    book.ReaderId = &reader.ID
+	book.IsIssued = true
+	book.ReaderId = &reader.ID
 
-    fmt.Printf("книга %s была выдана читателю %s %s\n", book.Title, reader.FirstName, reader.LastName)
-    return nil
+	return nil
 }
 
-
-func (lib *Library) ListAllBooks(){
-	for _, book  := range lib.Books{
+func (lib *Library) ListAllBooks() {
+	for _, book := range lib.Books {
 		fmt.Println(book.String())
-	} 
-}
- 
-//Выводит в консоль информацию о читателе
-func (r Reader) DisplayReader() {
-	fmt.Printf("Читатель: %s %s (ID: %d)\n", r.FirstName,r.LastName,r.ID)
+	}
 }
 
-//Dectivate делает читателя не активным
-func (r* Reader) Deactivate(){
+func (r *Reader) Deactivate() {
 	r.IsActive = false
 }
 
-func (r Reader) String() string{
+func (r Reader) String() string {
 	status := ""
 
-	if r.IsActive{
+	if r.IsActive {
 		status = "Активен"
 	} else {
 		status = "Не активен"
 	}
-	return fmt.Sprintf("Пользователь %s %s, ID: %d, пользователь:%s",r.FirstName,r.LastName,r.ID,status)
+	return fmt.Sprintf("Пользователь %s %s, ID: %d, пользователь:%s", r.FirstName, r.LastName, r.ID, status)
 }
 
 func (b Book) String() string {
-
 	status := "В библиотеке"
 	if b.IsIssued && b.ReaderId != nil {
 		status = fmt.Sprintf("На руках у читателя с ID: %d", *b.ReaderId)
@@ -145,37 +136,45 @@ func (b Book) String() string {
 	return fmt.Sprintf(`"%s (%s, %d), Статус: %s"`, b.Title, b.Author, b.Year, status)
 }
 
-func (r* Reader) AssignBook(b* Book){
-	fmt.Printf("Читатель %s %s взял книгу '%s' (%s, %d)\n", r.FirstName,r.LastName,b.Title, b.Author, b.Year)
+func (r *Reader) AssignBook(b *Book) error {
+
+	if b.IsIssued {
+		return fmt.Errorf("книга '%s' уже выдана", b.Title)
+	}
+	if !r.IsActive {
+		return fmt.Errorf("читатель %s %s не активен", r.FirstName, r.LastName)
+	}
+	
+	b.IsIssued = true
+	b.ReaderId = &r.ID
+	return nil
 }
 
-func (b *Book) IssuesBook(r *Reader) {
+func (b *Book) IssuesBook(r *Reader) error {
+
 	if b.IsIssued {
-		fmt.Printf("Книга %s уже кому-то выдана", b.Title)
-		return
-	}   
-	  if !r.IsActive {
-		fmt.Printf("Читатель %s %s не активен и не может получить книгу.", r.FirstName, r.LastName)
-		return
+		return fmt.Errorf("книга %s уже кому-то выдана", b.Title)
+	}
+	if !r.IsActive {
+		return fmt.Errorf("читатель %s %s не активен и не может получить книгу", r.FirstName, r.LastName)
 	}
 
 	b.IsIssued = true
 	b.ReaderId = &r.ID
-	fmt.Printf("Книга %s была выдана\n", b.Title)
+	return nil
 }
 
 func (b *Book) ReturnBook() error {
 	if !b.IsIssued {
 		return fmt.Errorf("книга '%s' и так в библиотеке", b.Title)
 	}
-	
+
 	b.IsIssued = false
 	b.ReaderId = nil
-	fmt.Printf("Книга %s возвращена в библиотеку\n", b.Title)
 	return nil
 }
 
-func (lib *Library) ReturnBook(bookID int) error{
+func (lib *Library) ReturnBook(bookID int) error {
 	book, err := lib.FindBookByID(bookID)
 	if err != nil {
 		return err
